cc746b33ab582a00b7960a440ef353549f6f9164
Tobias Knerr <osm@tobias-knerr.de>
2023-08-23 12:38:22 +0200

    Provide a first basic glTF tile viewer using ArcRotateCamera

---
+++ src/geocoords.js
@@ -0,0 +1,141 @@
+"use strict"
+
+/**
+ * coordinate pair with latitude and longitude in degrees
+ */
+class LatLon {
+    constructor(lat, lon) {
+        this.lat = lat
+        this.lon = lon
+    }
+}
+
+/**
+ * tile number with zoom level.
+ * Tile coords follow the common XYZ convention, with an Y axis that points southward.
+ */
+class TileNumber {
+
+    constructor(zoom, x, y) {
+
+        this.zoom = zoom
+        this.x = x
+        this.y = y
+
+        if (zoom < 0) {
+            throw new Error("illegal tile number, zoom must not be negative: " + toString());
+        } else if (x < 0 || y < 0) {
+            throw new Error("illegal tile number, x and y must not be negative: " + toString());
+        } else if (x >= (1 << zoom)) {
+            throw new Error("illegal tile number, x too large: " + toString());
+        } else if (y >= (1 << zoom)) {
+            throw new Error("illegal tile number, y too large: " + toString());
+        }
+
+    }
+
+    toString() {
+        return `${this.zoom}/${this.x}/${this.y}`
+    }
+
+    add(x, y) {
+        return new TileNumber(this.zoom, this.x + x, this.y + y)
+    }
+
+    /** returns the TileNumber at the given zoom and location */
+    static atLatLon(zoom, latLon) {
+        const x = Math.floor((latLon.lon + 180) / 360 * (1<<zoom));
+        const y = Math.floor((1 - Math.log(Math.tan(toRadians(latLon.lat)) + 1 / Math.cos(toRadians(latLon.lat))) / Math.PI) / 2 * (1<<zoom));
+        return new TileNumber(zoom, x, y);
+    }
+
+    bounds() {
+        const min = new LatLon(
+            TileNumber.tile2lat(this.y + 1, this.zoom),
+            TileNumber.tile2lon(this.x, this.zoom));
+        const max = new LatLon(
+            TileNumber.tile2lat(this.y, this.zoom),
+            TileNumber.tile2lon(this.x + 1, this.zoom));
+        const center = new LatLon((min.lat + max.lat) / 2, (min.lon + max.lon) / 2);
+        return {min: min, max: max, center: center};
+    }
+
+    static tile2lon(x, z) {
+        return x / Math.pow(2.0, z) * 360.0 - 180;
+    }
+
+    static tile2lat(y, z) {
+        const n = Math.PI - (2.0 * Math.PI * y) / Math.pow(2.0, z)
+        return toDegrees(Math.atan(Math.sinh(n)))
+    }
+
+}
+
+/**
+ * application of an orthographic projection that is intended to use values in meters centered around the coordinate
+ * center (0,0). It projects coordinates onto a plane touching the globe at the origin.
+ * This results in sufficient accuracy if the data covers only a small part of the globe.
+ */
+class OrthographicAzimuthalMapProjection {
+
+    constructor(origin) {
+        this.origin = origin;
+        this._lat0 = toRadians(origin.lat);
+        this._lon0 = toRadians(origin.lon);
+    }
+
+    toXZ(latLon) {
+
+        const lat = toRadians(latLon.lat);
+        const lon = toRadians(latLon.lon);
+
+        const x = GLOBE_RADIUS * Math.cos(lat) * Math.sin(lon - this._lon0);
+        const y = GLOBE_RADIUS * (Math.cos(this._lat0) * Math.sin(lat) - Math.sin(this._lat0) * Math.cos(lat) * Math.cos(lon - this._lon0));
+
+        return {x: x, y: y};
+
+    }
+
+    toLat(pos) {
+
+        const rho = Math.sqrt(pos.x * pos.x + pos.z * pos.z);
+        const c = Math.asin(rho / GLOBE_RADIUS);
+
+        if (rho > 0) {
+            return toDegrees(Math.asin( Math.cos(c) * Math.sin(this._lat0) + ( pos.z * Math.sin(c) * Math.cos(this._lat0) ) / rho ));
+        } else {
+            return toDegrees(this._lat0);
+        }
+
+    }
+
+    toLon(pos) {
+
+        const rho = Math.sqrt(pos.x * pos.x + pos.z * pos.z);
+        const c = Math.asin(rho / GLOBE_RADIUS);
+
+        const div = rho * Math.cos(this._lat0) * Math.cos(c) - pos.z * Math.sin(this._lat0) * Math.sin(c);
+
+        if (Math.abs(div) > 1e-5) {
+            return toDegrees(this._lon0 + Math.atan2( pos.x * Math.sin(c), div ));
+        } else {
+            return toDegrees(this._lon0);
+        }
+
+    }
+
+    toLatLon(pos) {
+        return new LatLon(this.toLat(pos), this.toLon(pos))
+    }
+
+}
+
+const GLOBE_RADIUS = 6371000;
+
+function toDegrees(angleInRadians) {
+    return angleInRadians * (180 / Math.PI)
+}
+
+function toRadians(angleInDegrees) {
+    return angleInDegrees * (Math.PI / 180);
+}
--- src/osm2world-frontend.js
+++ src/osm2world-frontend.js
@@ -4,7 +4,7 @@
  * Namespace for the OSM2World web frontend.
  * @namespace
  */
-var OSM2World = {};
+const OSM2World = {};
 (function() {
 
 	/** WebGL-based viewer */
@@ -12,11 +12,68 @@ var OSM2World = {};
 
 		/**
 		 * @param {string} canvasID  id of the canvas to use for the viewer
+		 * @param {string} tileRoot  root URL for 3D tiles in glTF format
 		 */
-		constructor(canvasID) {
+		constructor(canvasID, tileRoot) {
 
-			var canvas = document.getElementById(canvasID);
-			//renderer = new THREE.WebGLRenderer({ canvas: canvas });
+			const canvas = document.getElementById(canvasID);
+			canvas.setAttribute("touchAction", "none");
+
+			const engine = new BABYLON.Engine(canvas, true);
+
+			const createScene = function () {
+				const scene = new BABYLON.Scene(engine);
+
+				const camera = new BABYLON.ArcRotateCamera("camera", Math.PI / 2, Math.PI / 4, 500, new BABYLON.Vector3(0, 0, 0));
+				camera.attachControl(canvas, true);
+				camera.minZ = 10;
+				camera.maxZ = 10000;
+				camera.panningSensibility = 3;
+				const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0));
+
+				const urlParams = new URLSearchParams(window.location.search);
+				console.log(urlParams);
+
+				const lat = urlParams.get("lat") || 48.14738;
+				const lon = urlParams.get("lon") || 11.57403;
+				const originLatLon = new LatLon(lat, lon)
+				const proj = new OrthographicAzimuthalMapProjection(originLatLon)
+				const centerTile = TileNumber.atLatLon(15, originLatLon)
+
+				function loadAndPlaceTile(tileNumber) {
+					return BABYLON.SceneLoader.ImportMeshAsync(null, tileRoot, "lod1/" + tileNumber + ".glb").then((result) => {
+						const centerPos = proj.toXZ(tileNumber.bounds().center)
+						result.meshes[0].setAbsolutePosition(-centerPos.x, 0, -centerPos.y)
+					})
+				}
+
+				loadAndPlaceTile(centerTile, true).then(() => {
+					setTimeout(function () {
+						for (let x = -3; x <= 3; x++) {
+							for (let y = -3; y <= 3; y++) {
+								let dist = Math.abs(x) + Math.abs(y);
+								if (dist > 0 && dist <= 4) {
+									loadAndPlaceTile(centerTile.add(x, y))
+								}
+							}
+						}
+					}, 500);
+				})
+
+				return scene;
+			};
+
+			const scene = createScene(); //Call the createScene function
+
+			// Register a render loop to repeatedly render the scene
+			engine.runRenderLoop(function () {
+				scene.render();
+			});
+
+			// Watch for browser/canvas resize events
+			window.addEventListener("resize", function () {
+				engine.resize();
+			});
 
 		}
 
